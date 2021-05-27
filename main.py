2c43cdb02c3b8cb3aa18b80d8f62f06d0700717e
Tomasz Mianecki <>
2021-05-27 08:56:26 +0000

    Ukrócony makaron...

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def cleeenzaps():
         tab_Elektryczne.clear()
     if lm > 0:
         tab_Mechaniczne.clear()
-
+"""
 def zapisz_zebrane_dane():
     cleeenzaps()
     tab_Ogolne.append(l_E12.get())
@@ -67,7 +67,7 @@ def zapisz_zebrane_dane():
     print("Swietlne")
     print(tab_Swietlne)
     tab_Pozostale.append()
-
+"""
 def pokazSchemat():
     schem = Toplevel(okno)
     lab=Label(schem, text=tab_Ogolne)
@@ -77,9 +77,33 @@ def pokazSchemat():
     lab3=Label(schem, text=tab_Elektryczne)
     lab3.pack()
 
+def automatyczna_PozycjaWTabeli(strona,numer_pozycji,tekst,wsad_wejsciowy,pozycjaStartowaX,pozycjaStartowaY ,roznica_y,root,przesuniecie):
+    st=strona
+    nr = numer_pozycji
+    tx = tekst
+    ws=wsad_wejsciowy
+    pzS_X=pozycjaStartowaX
+    pzS_Y=pozycjaStartowaY
+    #x_plus=roznica_x
+    y_plus=roznica_y
+    przez=przesuniecie
+    starLabel= st + "_L"+str(nr)
+    print(starLabel)
+    starEntry =st + "_E"+str(nr)
+    print(starEntry)
+    starLabel = Label(root, text=tx)
+    xx = pzS_X # +x_plus
+    yy = pzS_Y+y_plus
+    starLabel.place(x=xx,y=yy)
+    starEntry=Entry(root, bd = 5)
+    starEntry.insert(0,ws)
+    xxx=xx+przez
+    starEntry.place(x=xxx,y=yy)
+    print("Label")
+    print("Entry")
 
 okno = tkinter.Tk()
-okno.geometry("1200x600")
+okno.geometry("1200x800")
 okno.title("Generator Kart ver 1.2")
 
 ico = Image.open('IkonGEN.png')
@@ -124,15 +148,12 @@ var = StringVar()
 
 #scrollbar = Scrollbar(okno)
 #scrollbar.pack( side = RIGHT, fill = Y )
-def sel():
-       selection = " " + str(var.get())
-       label.config(text = selection)
+#def sel():
+ #      selection = " " + str(var.get())
+ #      label.config(text = selection)
 
 
 teksty=["Zastosowanie","Cechy produktu","Zdjęcie","Rys. techniczny","Krzywa rozsyłu","Nagłówek","Produkt","Rodzina","Parametry"]
-
-
-
 
 # fundamenty aplikacji
 o_L1 = Label(okno, text = teksty[5])
@@ -200,293 +221,92 @@ o_Entry_tolerancja2.insert(0,"** tolerancja strumienia świetlnego 7% w temp. ot
 o_Entry_tolerancja2.place(x=10, y=450)
 
 
-
 # druga strona z parametrami
 # parametry
 o_L10 = Label(okno,text = teksty[8] ,fg="chocolate",relief = GROOVE)
 o_L10.place(x = 510, y = 50)
-# kolumna 1 , fragment 1/2  x= 450; fragment 2/2 x=600; roznica w "y" to 20
+# kolumna 1 , fragment 1/2  x= 450; fragment 2/2 x=650; roznica w "y" to 20
+
 l_L11 = Label(okno,text = "Ogólne", fg="green" ,relief = GROOVE)
 l_L11.place(x = 450, y = 70)
 
-l_L12 = Label(okno, text ="Kod produktu")
-l_L12.place(x = 450,y = 90)
+tab_Parametry_Ogolne = ["Kod produktu","Kod rodziny produktu","Kod barwy","Wbudowany zasilacz","Producent zasilacza","Sterowanie","Klasa ochronności","Stopień ochrony","Odporność na uderzenia","Kolor","Producent źródeł światła","Ilość źródeł światła "]
+x_Ogolne = 450
+y_Ogolne = 70
+przesuniecie = 150
+roznicaY = 30
+pozz= 1
+for x in tab_Parametry_Ogolne:
+    automatyczna_PozycjaWTabeli("l",pozz,x,"Null",x_Ogolne,y_Ogolne,roznicaY,okno,przesuniecie)
+    pozz+=1
+    y_Ogolne+=roznicaY
 
-l_E12 = Entry(okno, bd = 5)
-l_E12.insert(0,"NULL")
-l_E12.place(x = 600,y = 90)
-
-l_L13 = Label(okno, text ="Kod rodziny produktu")
-l_L13.place(x = 450,y = 110)
-
-l_E13 = Entry(okno, bd = 5)
-l_E13.insert(0,"NULL")
-l_E13.place(x = 600,y = 110)
-
-l_L14 = Label(okno, text ="Kod barwy")
-l_L14.place(x = 450,y = 130)
-
-l_E14 = Entry(okno, bd = 5)
-l_E14.insert(0,"NULL")
-l_E14.place(x = 600,y = 130)
-
-l_L15 = Label(okno, text ="Wbudowany zasilacz")
-l_L15.place(x = 450,y = 150)
-
-l_w = Radiobutton ( okno, text = "Tak", variable = var,  value = "Tak"  ,command = sel  )
-l_w.place(x=600,y=150)
-
-l_w2 = Radiobutton ( okno, text = "Nie", variable = var,  value = "Nie"  ,command = sel )
-l_w2.place(x=630,y=150)
-
-label = Label(okno,fg="green")
-label.place(x=680,y=150)
-
-l_L16 = Label(okno, text ="Producent zasilacza")
-l_L16.place(x = 450,y = 170)
-
-l_E16 = Entry(okno, bd = 5)
-l_E16.insert(0,"NULL")
-l_E16.place(x = 600,y = 170)
-
-l_L17 = Label(okno, text ="Sterowanie")
-l_L17.place(x = 450,y = 190)
-
-l_E17 = Entry(okno, bd = 5)
-l_E17.insert(0,"NULL")
-l_E17.place(x = 600,y = 190)
-
-l_L18 = Label(okno, text ="Klasa ochronności")
-l_L18.place(x = 450,y = 210)
-
-l_E18 = Entry(okno, bd = 5)
-l_E18.insert(0,"NULL")
-l_E18.place(x = 600,y = 210)
-
-l_L19 = Label(okno, text ="Stopień ochrony")
-l_L19.place(x = 450,y = 230)
-
-l_E19 = Entry(okno, bd = 5)
-l_E19.insert(0,"NULL")
-l_E19.place(x = 600,y = 230)
-
-l_L20 = Label(okno, text ="Odporność na uderzenia")
-l_L20.place(x = 450,y = 250)
-
-l_E20 = Entry(okno, bd = 5)
-l_E20.insert(0,"NULL")
-l_E20.place(x = 600,y = 250)
-
-l_L21 = Label(okno, text ="Kolor")
-l_L21.place(x = 450,y = 270)
-
-l_E21 = Entry(okno, bd = 5)
-l_E21.insert(0,"NULL")
-l_E21.place(x = 600,y = 270)
-
-l_L22 = Label(okno, text ="Producent źródeł światła")
-l_L22.place(x = 450,y = 290)
-
-l_E22 = Entry(okno, bd = 5)
-l_E22.insert(0,"NULL")
-l_E22.place(x = 600,y = 290)
-
-l_L23 = Label(okno, text ="Ilość źródeł światła ")
-l_L23.place(x = 450,y = 310)
-
-l_E23 = Entry(okno, bd = 5)
-l_E23.insert(0,"NULL")
-l_E23.place(x = 600,y = 310)
 
 l_L24 = Label(okno,text = "Elektryczne",fg="green" ,relief = GROOVE)
-l_L24.place(x = 450, y = 330)
+y_Ogolne+=roznicaY
+l_L24.place(x = x_Ogolne, y = y_Ogolne)
 
-l_L25 = Label(okno, text ="Napięcie znamionowe")
-l_L25.place(x = 450,y = 350)
+tab_Parametry_Elektryczne =["Napięcie znamionowe","Częstotliwość sieciowa "]
 
-l_E25 = Entry(okno, bd = 5)
-l_E25.insert(0,"NULL")
-l_E25.place(x = 600,y = 350)
+for x in tab_Parametry_Elektryczne:
+    automatyczna_PozycjaWTabeli("l",pozz,x,"Null",x_Ogolne,y_Ogolne,roznicaY,okno,przesuniecie)
+    pozz+=1
+    y_Ogolne+=roznicaY
 
-l_L26 = Label(okno, text ="Częstotliwość sieciowa ")
-l_L26.place(x = 450,y = 370)
-
-l_E26 = Entry(okno, bd = 5)
-l_E26.insert(0,"NULL")
-l_E26.place(x = 600,y = 370)
 
 # kolumna 2 fragment 1/2  x = 750; fragment 2/2 x=950; roznica w "y" to 20
+x_OgolnePrawe= 750
+y_Ogolne = 70
+pozz = 1
+tab_Parametry_Swietlne=["Moc znamionowa","Strumień świetlny oprawy","Wydajność oprawy","CRI/Ra","Temperatura barwowa","Barwa światła","Kąt rozsyłu"]
+
 p_L13 = Label(okno,text = "Świetlne", fg="green", relief = GROOVE)
 p_L13.place(x = 750, y = 70)
 
-p_L14 = Label(okno, text ="Moc znamionowa")
-p_L14.place(x = 750,y = 90)
 
-p_E14 = Entry(okno, bd = 5)
-p_E14.insert(0,"NULL")
-p_E14.place(x = 950,y = 90)
+for x in tab_Parametry_Swietlne:
+    automatyczna_PozycjaWTabeli("p",pozz,x,"Null",x_OgolnePrawe,y_Ogolne,roznicaY,okno,przesuniecie)
+    pozz+=1
+    y_Ogolne+=roznicaY
 
-p_L15 = Label(okno, text ="Strumień świetlny oprawy")
-p_L15.place(x = 750,y = 110)
 
-p_E15 = Entry(okno, bd = 5)
-p_E15.insert(0,"NULL")
-p_E15.place(x = 950,y = 110)
-
-p_L16 = Label(okno, text ="Wydajność oprawy")
-p_L16.place(x = 750,y = 130)
-
-p_E16 = Entry(okno, bd = 5)
-p_E16.insert(0,"NULL")
-p_E16.place(x = 950,y = 130)
-
-p_L17 = Label(okno, text ="CRI/Ra")
-p_L17.place(x = 750,y = 150)
-
-p_E17 = Entry(okno, bd = 5)
-p_E17.insert(0,"NULL")
-p_E17.place(x = 950,y = 150)
-
-p_L18 = Label(okno, text ="Temperatura barwowa")
-p_L18.place(x = 750,y = 170)
-
-p_E18 = Entry(okno, bd = 5)
-p_E18.insert(0,"NULL")
-p_E18.place(x = 950,y = 170)
-
-p_L19 = Label(okno, text ="Barwa światła")
-p_L19.place(x = 750,y = 190)
-
-p_E19 = Entry(okno, bd = 5)
-p_E19.insert(0,"NULL")
-p_E19.place(x = 950,y = 190)
-
-p_L20 = Label(okno, text ="Kąt rozsyłu")
-p_L20.place(x = 750,y = 210)
-
-p_E20 = Entry(okno, bd = 5)
-p_E20.insert(0,"NULL")
-p_E20.place(x = 950,y = 210)
 
 p_L21 = Label(okno,text = "Pozostałe" ,fg="green", relief = GROOVE)
-p_L21.place(x = 750, y = 230)
+y_Ogolne+=roznicaY
+p_L21.place(x = x_OgolnePrawe, y = y_Ogolne)
 
-p_L22 = Label(okno, text ="Waga")
-p_L22.place(x = 750,y = 250)
+tab_Parametry_Pozostale = ["Waga","Żywotność","Żywotność L90B10","Żywotność L80B10","Żywotność L70B10","Sposób montażu ","Zakres temp. otoczenia "]
 
-p_E22 = Entry(okno, bd = 5)
-p_E22.insert(0,"NULL")
-p_E22.place(x = 950,y = 250)
+for x in tab_Parametry_Pozostale:
+    automatyczna_PozycjaWTabeli("p",pozz,x,"Null",x_OgolnePrawe,y_Ogolne,roznicaY,okno,przesuniecie)
+    pozz+=1
+    y_Ogolne+=roznicaY
 
-p_L23 = Label(okno, text ="Żywotność")
-p_L23.place(x = 750,y = 270)
 
-p_E23 = Entry(okno, bd = 5)
-p_E23.insert(0,"NULL")
-p_E23.place(x = 950,y = 270)
-
-p_L24 = Label(okno, text ="Żywotność L90B10")
-p_L24.place(x = 750,y = 290)
-
-p_E24 = Entry(okno, bd = 5)
-p_E24.insert(0,"NULL")
-p_E24.place(x = 950,y = 290)
-
-p_L25 = Label(okno, text ="Żywotność L80B10")
-p_L25.place(x = 750,y = 310)
-
-p_E25 = Entry(okno, bd = 5)
-p_E25.insert(0,"NULL")
-p_E25.place(x = 950,y = 310)
-
-p_L26 = Label(okno, text ="Żywotność L70B10")
-p_L26.place(x = 750,y = 330)
-
-p_E26 = Entry(okno, bd = 5)
-p_E26.insert(0,"NULL")
-p_E26.place(x = 950,y = 330)
-
-p_L27 = Label(okno, text ="Sposób montażu ")
-p_L27.place(x = 750,y = 350)
-
-p_E27 = Entry(okno, bd = 5)
-p_E27.insert(0,"NULL")
-p_E27.place(x = 950,y = 350)
-
-p_L28 = Label(okno, text ="Zakres temperatur otoczenia ")
-p_L28.place(x = 750,y = 370)
-
-p_E28 = Entry(okno, bd = 5)
-p_E28.insert(0,"NULL")
-p_E28.place(x = 950,y = 370)
 
 
 p_L29 = Label(okno,text = "Mechaniczne",fg="green" , relief = GROOVE)
-p_L29.place(x = 750, y = 390)
+y_Ogolne+=roznicaY
+p_L29.place(x = x_OgolnePrawe, y = y_Ogolne)
 
-p_L30 = Label(okno, text ="Materiał obudowy")
-p_L30.place(x = 750,y = 410)
+tab_Parametry_Mechaniczne=["Materiał obudowy","Materiał klosza","Wymiary (A x B x C mm)","Wymiary (Ø x D mm)"]
 
-p_E30 = Entry(okno, bd = 5)
-p_E30.insert(0,"NULL")
-p_E30.place(x = 950,y = 410)
-
-p_L31 = Label(okno, text ="Materiał klosza")
-p_L31.place(x = 750,y = 430)
-
-p_E31 = Entry(okno, bd = 5)
-p_E31.insert(0,"NULL")
-p_E31.place(x = 950,y = 430)
-
-p_L32 = Label(okno, text ="Wymiary (A x B x C mm)")
-p_L32.place(x = 750,y = 450)
-
-p_E32 = Entry(okno, bd = 5)
-p_E32.insert(0,"NULL")
-p_E32.place(x = 950,y = 450)
+for x in tab_Parametry_Mechaniczne:
+    automatyczna_PozycjaWTabeli("p",pozz,x,"Null",x_OgolnePrawe,y_Ogolne,roznicaY,okno,przesuniecie)
+    pozz+=1
+    y_Ogolne+=roznicaY
 
 
-p_L33 = Label(okno, text ="Wymiary (Ø x D mm)")
-p_L33.place(x = 750,y = 470)
-
-p_E33 = Entry(okno, bd = 5)
-p_E33.insert(0,"NULL")
-p_E33.place(x = 950,y = 470)
 
 
-def automatyczna_PozycjaWTabeli(strona,numer_pozycji,tekst,wsad_wejsciowy,pozycjaStartowaX,pozycjaStartowaY ,roznica_x,roznica_y,root,przesuniecie):
-    st=strona
-    nr = numer_pozycji
-    tx = tekst
-    ws=wsad_wejsciowy
-    pzS_X=pozycjaStartowaX
-    pzS_Y=pozycjaStartowaY
-    x_plus=roznica_x
-    y_plus=roznica_y
-    przez=przesuniecie
-    starLabel= st + "_L"+str(nr)
-    print(starLabel)
-    starEntry =st + "_E"+str(nr)
-    print(starEntry)
-    starLabel = Label(root, text=tx)
-    xx = pzS_X+x_plus
-    yy = pzS_Y+y_plus
-    starLabel.place(x=xx,y=yy)
-    starEntry=Entry(root, bd = 5)
-    starEntry.insert(0,ws)
-    xxx=xx+przez
-    starEntry.place(x=xxx,y=yy)
-    print("Label")
-    print("Entry")
-
-automatyczna_PozycjaWTabeli("l",40,"testowanie automatu","NULL",200,500,20,20,okno,200)
+# automatyczna_PozycjaWTabeli("l",40,"testowanie automatu","NULL",200,500,20,okno,150)
 
 # guziki do kreacji danych
-btn_submit = Button(okno,text = "Zapisz dane karty ", relief=RAISED, command = zapisz_zebrane_dane)
-btn_submit.place(x=1000,y=530)
+#btn_submit = Button(okno,text = "Zapisz dane karty ", relief=RAISED, command = zapisz_zebrane_dane)
+#btn_submit.place(x=1000,y=530)
 
-btn_pokaz = Button(okno,text = "Pokaz wynik ", relief=RAISED, command = pokazSchemat)
-btn_pokaz.place(x=920,y=530)
+#btn_pokaz = Button(okno,text = "Pokaz wynik ", relief=RAISED, command = pokazSchemat)
+#btn_pokaz.place(x=920,y=530)
 
 
 
